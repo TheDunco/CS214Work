dd1cffe8c2d79371f9439fc9fa61e320b2f18fde
= <=>
2020-03-05 14:59:11 -0500

    Did lab05

---
+++ 214/labs/05/ruby/split.rb
@@ -0,0 +1,42 @@
+# split.rb splits a string into two substrings.
+#
+# Input: A string and an integer string position to split the string by.
+# Precondition: The position integer is within the range of the string size.
+# Output: The substrings aString(0, position - 1) and
+#   aString(pos, length() - 1).
+#
+# Begun by: Prof. Adams, for CS 214 at Calvin College.
+# Completed by: Duncan Van Keulen
+# Date: 3-5-2020
+#################################################################
+
+
+# split() splits a string into its two substrings.
+# Receive: aString, the string to be split,       
+#        pos, the split position.                  
+# PRE: 0 <= pos < the length of aString.
+# Return: the substring of aString before pos, and  
+#         the substring of aString from pos onwards.
+######################################################
+
+def split(a_string, pos)
+  # Split and assign first_part
+  first_part = a_string[0, pos]
+  # Split and assign last_part
+  last_part = a_string[pos, a_string.size()]
+
+  # Return an array with the first part and last part
+  retArr = Array[first_part, last_part]
+end
+
+def main
+  print "To split a string, enter the string: "; 
+  the_string = gets
+  print "Enter the split position: "; 
+  position = gets.to_i
+  split_string = split(the_string, position)
+  puts "The first part is: " << split_string[0]
+  puts "and the second part is: " << split_string[1]
+end
+
+main
