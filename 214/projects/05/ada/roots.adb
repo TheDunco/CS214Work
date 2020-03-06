af8c0687012217c82ee26c6fb922ea26b25fb1e0
= <=>
2020-03-06 14:19:26 -0500

    Just started ada

---
+++ 214/projects/05/ada/roots.adb
@@ -0,0 +1,19 @@
+-- roots.adb calculates quadratic roots.
+--
+-- Input: 
+--        
+-- Precondition: 
+-- Output: 
+--
+-- By: Duncan Van Keulen
+-- Date: 3-6-2020
+--------------------------------------------------------------
+
+with Ada.Float_IO;
+use Ada.Float_IO;
+
+procedure roots is
+
+    A, B, C : Double;
+
+
