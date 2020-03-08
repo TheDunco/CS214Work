e8cfdf5cd21d613c37faf714edb8a4098f90bea4
TheDunco <duncvan@gamil.com>
2020-03-08 19:05:33 -0400

    Did some testing with ada

--- 214/projects/05/ada/roots.adb
+++ 214/projects/05/ada/roots.adb
@@ -9,11 +9,33 @@
 -- Date: 3-6-2020
 --------------------------------------------------------------
 
-with Ada.Float_IO;
-use Ada.Float_IO;
+with Ada.Float_Text_IO;
+use Ada.Float_Text_IO;
+with Ada.Numerics.Generic_Elementary_Functions;
 
 procedure roots is
 
     A, B, C : Double;
+
+procedure roots(A : in Float; B : in Float; C : in Float) is
+begin
+    Put("Got'eem");
+
+end roots;
+
+begin
+    -- Get inputs of a, b, and c
+    Put("To get the roots of a quadratic function...\nEnter a: ");
+    Get(A);
+    Put("\nEnter b: ");
+    Get(B);
+    Put("\nEnter c: ");
+    Get(C);
+
+end roots;
+
+
+
+
 
 
--- 214/projects/05/java/Roots.java
+++ 214/projects/05/java/Roots.java
@@ -14,43 +14,41 @@ public class Roots {
         // Get inputs for a, b, c
         System.out.print("Quadratic Root Finder\nEnter a: ");
         double a = keyboard.nextDouble();
-    
+
         System.out.print("Enter b: ");
         double b = keyboard.nextDouble();
-    
+
         System.out.print("Enter c: ");
         double c = keyboard.nextDouble();
 
         double[] rootsArr = new double[2];
 
         // Only display roots if they are valid
-        if(root(a, b, c, rootsArr)) {
+        if (root(a, b, c, rootsArr)) {
             System.out.println("The roots are " + rootsArr[0] + " and " + rootsArr[1]);
         }
     }
 
-    /* 
-    * root() calculates the quadratic roots of an equation using the quadratic formula
-    * will return true if calculation succeeded and false if it failed.
-    * double a, b, c are the a, b, c numbers in the quadratic formula
-    * double[] rootsArr is the "pass by reference" array that will contain the roots
-    *******************************************************************************/
+    /*
+     * root() calculates the quadratic roots of an equation using the quadratic
+     * formula will return true if calculation succeeded and false if it failed.
+     * double a, b, c are the a, b, c numbers in the quadratic formula double[]
+     * rootsArr is the "pass by reference" array that will contain the roots
+     *******************************************************************************/
     public static boolean root(double a, double b, double c, double[] rootsArr) {
         if (a != 0) {
             double arg = Math.pow(b, 2.0) - 4 * a * c;
-            if (arg >=0) {
-                rootsArr[0] = (-b + Math.sqrt(arg))/(2*a);
-                rootsArr[1] = (-b - Math.sqrt(arg))/(2*a);
+            if (arg >= 0) {
+                rootsArr[0] = (-b + Math.sqrt(arg)) / (2 * a);
+                rootsArr[1] = (-b - Math.sqrt(arg)) / (2 * a);
                 return true;
-            }
-            else {
+            } else {
                 System.out.println("root(): b^2 - 4ac is negative!");
                 rootsArr[0] = 0.0;
                 rootsArr[1] = 0.0;
                 return false;
             }
-        }
-        else {
+        } else {
             System.out.println("root(): a is zero!");
             rootsArr[0] = 0.0;
             rootsArr[1] = 0.0;
@@ -58,5 +56,3 @@ public class Roots {
         }
     }
 }
-
-
