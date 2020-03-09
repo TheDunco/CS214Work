535a222e2b57fea04976d0d9a1ed1ccf09628147
= <=>
2020-03-09 18:11:13 -0400

    Pre-Ada-reboot

--- 214/projects/05/ada/roots.adb
+++ 214/projects/05/ada/roots.adb
@@ -9,15 +9,23 @@
 -- Date: 3-6-2020
 --------------------------------------------------------------
 
-with Ada.Float_Text_IO;
-use Ada.Float_Text_IO;
-with Ada.Numerics.Generic_Elementary_Functions;
+with Ada.Float_Text_IO; Ada.Text_IO; Ada.Numerics.Generic_Elementary_Functions;
+use Ada.Float_Text_IO; Ada.Text_IO; Ada.Numerics.Generic_Elementary_Functions;
 
 procedure roots is
 
-    A, B, C : Double;
+    A, B, C, root1, root2 : Float;
 
-procedure roots(A : in Float; B : in Float; C : in Float) is
+------------------------------------------------
+--  split() splits a string in two.           
+-- Receive: The_String, the string to be split,
+--          Position, the split index.        
+-- PRE: 0 < Position <= The_String.length(). 
+--     (Ada arrays are 1-relative by default)
+-- Passback: First_Part - the first substring,
+--           Last_Part - the second substring.
+------------------------------------------------
+procedure roots(A : in Float; B : in Float; C : in Float; root1 : out Float; root2 : out Float ) is
 begin
     Put("Got'eem");
 
@@ -31,6 +39,13 @@ begin
     Get(B);
     Put("\nEnter c: ");
     Get(C);
+
+    roots(A, B, C, root1, root2);
+
+    Put("The roots are: ");
+    Put(root1);
+    Put(" and ");
+    Put(root2);
 
 end roots;
 
