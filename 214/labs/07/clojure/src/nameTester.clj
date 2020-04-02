a30f51cfa13ca0dd944b7339755ef85ee2091480
= <=>
2020-04-02 12:25:19 -0400

    up to accessors in clojure

--- 214/labs/07/clojure/src/nameTester.clj
+++ 214/labs/07/clojure/src/nameTester.clj
@@ -14,7 +14,7 @@
 ;;; define a Clojure record named Name 
 ;;; (which compiles to a Java class).
 
-(defrecord Name [myFirst myMiddle myLast])
+(defrecord Name [firstName middleName lastName])
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -23,7 +23,9 @@
 ;;; Return: the Name (firstN middleN lastN). 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-; Replace this line with a definition of function make-Name
+(defn make-Name [first middle last] 
+  (->Name first middle last)
+)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; getFirst() extracts the first name of a Name object.
@@ -38,7 +40,7 @@
 ;;; Receive: aName, a Name. 
 ;;; Return: the middleName string in aName.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- 
+
 ; Replace this line with the definition of getMiddle()
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -46,7 +48,7 @@
 ;;; Receive: aName, a Name. 
 ;;; Return: the lastName string in aName.
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
- 
+
 ; Replace this line with the definition of getLast()
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -73,35 +75,35 @@
 (defn -main []
   (let
     [                                          ; 3 ways to construct an object:
-;      name1 (make-Name "John" "Paul" "Jones")  ; -using our "make-" constructor
-;      name2 (->Name "Jane" "Penelope" "Jones") ; -invoking constructor directly
-                                               ; -mapping field-names to values
-;      name3 (map->Name {:lastName "Jones" :firstName "Jinx" :middleName "Joy"})
+    name1 (make-Name "John" "Paul" "Jones")  ; -using our "make-" constructor
+    name2 (->Name "Jane" "Penelope" "Jones") ; -invoking constructor directly
+                                                ; -mapping field-names to values
+    name3 (map->Name {:lastName "Jones" :firstName "Jinx" :middleName "Joy"})
     ]
     ;; ----- SECTION 1 -----
-;    (println)
-;    (print name1) (println)
-;    (assert (= (getFirst name1) "John") "getFirst(1) failed")
-;    (assert (= (getMiddle name1) "Paul") "getMiddle(1) failed")
-;    (assert (= (getLast name1) "Jones") "getLast(1) failed")
-;    (assert (= (toString name1) "John Paul Jones") "toString(1) failed")
-;    (printName name1) (println)
+    (println)
+    (print name1) (println)
+    ; (assert (= (getFirst name1) "John") "getFirst(1) failed")
+    ; (assert (= (getMiddle name1) "Paul") "getMiddle(1) failed")
+    ; (assert (= (getLast name1) "Jones") "getLast(1) failed")
+    ; (assert (= (toString name1) "John Paul Jones") "toString(1) failed")
+    ; (printName name1) (println)
     ;; ----- SECTION 2 -----
-;    (println)
-;    (print name2) (println)
-;    (assert (= (getFirst name2) "Jane") "getFirst(2) failed")
-;    (assert (= (getMiddle name2) "Penelope") "getMiddle(2) failed")
-;    (assert (= (getLast name2) "Jones") "getLast(2) failed")
-;    (assert (= (toString name2) "Jane Penelope Jones") "toString(2) failed")
-;    (printName name2) (println)
+    (println)
+    (print name2) (println)
+    ; (assert (= (getFirst name2) "Jane") "getFirst(2) failed")
+    ; (assert (= (getMiddle name2) "Penelope") "getMiddle(2) failed")
+    ; (assert (= (getLast name2) "Jones") "getLast(2) failed")
+    ; (assert (= (toString name2) "Jane Penelope Jones") "toString(2) failed")
+    ; (printName name2) (println)
     ;; ----- SECTION 3 -----
-;    (println)
-;    (print name3) (println)
-;    (assert (= (getFirst name3) "Jinx") "getFirst(3) failed")
-;    (assert (= (getMiddle name3) "Joy") "getMiddle(3) failed")
-;    (assert (= (getLast name3) "Jones") "getLast(3) failed")
-;    (assert (= (toString name3) "Jinx Joy Jones") "toString(3) failed")
-;    (printName name3) (println)
+    (println)
+    (print name3) (println)
+    ; (assert (= (getFirst name3) "Jinx") "getFirst(3) failed")
+    ; (assert (= (getMiddle name3) "Joy") "getMiddle(3) failed")
+    ; (assert (= (getLast name3) "Jones") "getLast(3) failed")
+    ; (assert (= (toString name3) "Jinx Joy Jones") "toString(3) failed")
+    ; (printName name3) (println)
 
     (println "\nAll tests passed!\n")
   )
