07fd90bde5140931f175bbd86ffa0e9b3709561a
= <=>
2020-04-13 19:04:09 -0400

    Proj08 done!

--- 214/projects/08/ruby/Temperature.rb
+++ 214/projects/08/ruby/Temperature.rb
@@ -42,13 +42,32 @@ module TemperatureModule
             end #case
         end #isValidTemperature
 
+        ###################################################################
+        # initialize instantiates a Temperature object's myDegree and myScale values
+        # Receive: degree : number - the desired degree of the Temperature
+        # Recieve: scale : string/character - the desired scale of that degree        
+        # Postcondition: Temperature object is initialized with myDegree and myScale
+        ###################################################################
+
         def initialize(degree, scale)
             if isValidTemperature(degree, scale)
                 @myDegree, @myScale = degree, scale
             end
         end
 
+        ###################################################################
+        # attr_reader defines the read or "get" methods for myDegree and myScale       
+        # Return: myDegree or myScale of Temperature object
+        ###################################################################
+
         attr_reader :myDegree, :myScale
+
+        ###################################################################
+        # raiseTemperature raises the degree of a Temperature by degrees
+        # Receive: degrees : number - the number of degrees to raise by
+        # Precondition: @myDegree + degrees must constitute a valid Temperature
+        # Postcondition: @myDegree will be raised degrees
+        ###################################################################
 
         def raiseTemperature(degrees)
             if isValidTemperature((self.myDegree + degrees), @myScale)
@@ -58,6 +77,13 @@ module TemperatureModule
             end #if
         end #raiseTemperature
 
+        ###################################################################
+        # lowerTemperature lowers the degree of a Temperature by degrees
+        # Receive: degrees : number - the number of degrees to be lowerd by
+        # Precondition: @myDegree - degrees must constitute a valid Temperature
+        # Postcondition: @myDegree will be lowered degrees
+        ###################################################################
+
         def lowerTemperature(degrees)
             if isValidTemperature((@myDegrees - degrees), @myScale)
                 return Temperature.new((@myDegree - degrees), @myScale)
@@ -65,6 +91,12 @@ module TemperatureModule
                 puts "Could not raise temperature"
             end #if
         end #lowerTemperature
+
+        ###################################################################
+        # toFahrenheit() converts this Temperature to Fahrenheit from an arbitrary scale
+        # Precondition: myDegree must be a valid degree in on of F, C, K scales
+        # Return: A new Temperature object with converted degree and scale  
+        ###################################################################
 
         def toFahrenheit()
             case @myScale
@@ -83,6 +115,12 @@ module TemperatureModule
             end #case
         end #toFahrenheit
 
+        ###################################################################
+        # toCelsius() converts this Temperature to Celsius from an arbitrary scale
+        # Precondition: myDegree must be a valid degree in on of F, C, K scales
+        # Return: A new Temperature object with converted degree and scale  
+        ###################################################################
+
         def toCelsius()
             case @myScale
             when 'F', 'f'
@@ -99,6 +137,12 @@ module TemperatureModule
                 return false
             end #case
         end #toCelsius
+
+        ###################################################################
+        # toKelvin() converts this Temperature to Kelvin from an arbitrary scale
+        # Precondition: myDegree must be a valid degree in on of F, C, K scales
+        # Return: A new Temperature object with converted degree and scale  
+        ###################################################################
 
         def toKelvin()
             case @myScale
@@ -117,6 +161,17 @@ module TemperatureModule
             end #case
         end #toFahrenheit
 
+        ###################################################################
+        # enteTemperature allows a Temperature object's myDegree and myScale
+        #       to be set to user entered values
+        # Receive: A single string, entered by the user
+        # Precondition: the input string must be in the form "#'s scale"
+        #   where #'s is a number and scale is a single character representing
+        #   the scale of the Temperature       
+        # Return: A new Temperature object where degree and scale are set to
+        #       the user-entered values
+        ###################################################################
+
         def enterTemperature()
             input = gets.split
             degree = input[0].to_f
@@ -125,13 +180,32 @@ module TemperatureModule
             return Temperature.new(degree, scale)
         end
 
+        ###################################################################
+        # displayTemperature() allows a Temperature object to be conveniently
+        #       displayed (printed) to the screen      
+        # Postcondition: This Temperature will be conveniently formatted and printed
+        ###################################################################
+
         def displayTemperature()
             print '%.5f' % @myDegree + " " + @myScale + "\t"
         end
 
+        ###################################################################
+        # equals() allows two Temperatures to be compared (equality)
+        # Receive: temp : Temperature - the rhs of the == operation
+        # Return: a boolean indicating if this and temp have the 
+        #       same temperature (regardless of scale)
+        ###################################################################
+
         def equals(temp)
             return self.toFahrenheit().myDegree == temp.toFahrenheit.myDegree
         end
+
+        ###################################################################
+        # lessThan() allows two Temperatures to be compared (less than)
+        # Receive: temp : Temperature - the rhs of the < operation
+        # Return: a boolean indicating if this < temp (regardless of scale)
+        ###################################################################
 
         def lessThan(temp)
             return self.toFahrenheit().myDegree < temp.toFahrenheit.myDegree
