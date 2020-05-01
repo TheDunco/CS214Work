996f71d808a4565a75d017b00434abd867a0ae19
= <=>
2020-05-01 18:45:08 -0400

    Finished lab11

---
+++ 214/labs/11/ruby/singleTimeCommands.sh
@@ -0,0 +1,36 @@
+echo "Running commands to test single array_sum.rb"
+
+echo "========== 5numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt
+
+echo "========== 1000numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt
+
+echo "========== 10000numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt
+
+echo "========== 100000numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt
+
+echo "========== 1000000numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt
+
+echo "========== 10000000numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt
+
+echo "========== 100000000numbers.txt =========="
+ruby array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt
+ruby array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt
---
+++ 214/labs/11/ruby/threadedTimeCommands.sh
@@ -0,0 +1,155 @@
+echo "Running threaded_array_sum.rb on all files in .../numbers/ directory with 1-5 threads"
+
+echo " ========== 5numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/5numbers.txt 5
+
+echo " ========== 1000numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000numbers.txt 5
+
+echo " ========== 10000numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000numbers.txt 5
+
+echo " ========== 100000numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000numbers.txt 5
+
+echo " ========== 1000000numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/1000000numbers.txt 5
+
+echo " ========== 10000000numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/10000000numbers.txt 5
+
+echo " ========== 100000000numbers.txt ========== "
+echo " ---------- 1 Thread ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 1
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 1
+echo " ---------- 2 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 2
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 2
+echo " ---------- 3 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 3
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 3
+echo " ---------- 4 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 4
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 4
+echo " ---------- 5 Threads ---------- "
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 5
+ruby threaded_array_sum.rb /home/cs/214/labs/11/numbers/100000000numbers.txt 5
